3e8da1b908530bae9f2f208abce64255cf7fb0fb
Pedro Silva <147632665+pedroswitch@users.noreply.github.com>
2024-07-11 09:32:54 +0100

    Date: altered the attribute type to LocalDate.

--- src/main/java/com/example/personalfinance/domain/valueobjects/Date.java
+++ src/main/java/com/example/personalfinance/domain/valueobjects/Date.java
@@ -3,10 +3,11 @@ package com.example.personalfinance.domain.valueobjects;
 import com.example.personalfinance.ddd.ValueObject;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import java.time.LocalDate;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class Date extends ValueObject
 {
-    private final Date d;
+    private final LocalDate d;
 }
