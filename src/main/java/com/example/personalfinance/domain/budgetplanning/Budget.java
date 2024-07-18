6412cdc0ea4926db834b9087bea749cc0cfc71dc
Pedro Silva <147632665+pedroswitch@users.noreply.github.com>
2024-07-18 12:37:37 +0100

    Added Budget aggregate with tests.

---
+++ src/main/java/com/example/personalfinance/domain/budgetplanning/Budget.java
@@ -0,0 +1,36 @@
+package com.example.personalfinance.domain.budgetplanning;
+
+import com.example.personalfinance.ddd.AggregateRoot;
+import com.example.personalfinance.domain.valueobjects.BudgetCategory;
+import com.example.personalfinance.domain.valueobjects.BudgetId;
+import com.example.personalfinance.domain.valueobjects.Values;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class Budget implements AggregateRoot<BudgetId>
+{
+    private final BudgetId id;
+    private final BudgetCategory category;
+    private final Values value;
+
+    @Override
+    public BudgetId identity()
+    {
+        return this.id;
+    }
+
+    @Override
+    public boolean sameAs(Object object)
+    {
+        if ( this == object ) return true;
+        if ( object == null || getClass() != object.getClass() ) return false;
+
+        Budget that = (Budget) object;
+
+        if (!getId().equals(that.getId())) return false;
+        if (!getCategory().equals(that.getCategory())) return false;
+        return (getValue().equals(that.getValue()));
+    }
+}
---
+++ src/test/java/com/example/personalfinance/domain/budgetplanning/BudgetTest.java
@@ -0,0 +1,68 @@
+package com.example.personalfinance.domain.budgetplanning;
+
+import com.example.personalfinance.domain.valueobjects.BudgetCategory;
+import com.example.personalfinance.domain.valueobjects.BudgetId;
+import com.example.personalfinance.domain.valueobjects.Values;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BudgetTest
+{
+    BudgetId id;
+    BudgetCategory category;
+    Values value;
+
+    @BeforeEach
+    void setup()
+    {
+        id = new BudgetId(1L);
+        category = new BudgetCategory("Viaturas");
+        value = new Values(120.00);
+    }
+
+    @Test
+    void createBudget()
+    {
+        // Act
+        Budget budget = new Budget(id, category, value);
+
+        // Assert
+        assertNotNull(budget);
+    }
+
+    @Test
+    void shouldBeSameAs()
+    {
+        // Arrange
+        BudgetId id2 = new BudgetId(1L);
+        BudgetCategory category2 = new BudgetCategory("Viaturas");
+        Values value2 = new Values(120.00);
+        Budget budget2 = new Budget(id2, category2, value2);
+        Budget budget = new Budget(id, category, value);
+
+        // Act
+        boolean result = budget.sameAs(budget2);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldNotBeSameAs()
+    {
+        // Arrange
+        BudgetId id2 = new BudgetId(2L);
+        BudgetCategory category2 = new BudgetCategory("Viaturas");
+        Values value2 = new Values(120.00);
+        Budget budget2 = new Budget(id2, category2, value2);
+        Budget budget = new Budget(id, category, value);
+
+        // Act
+        boolean result = budget.sameAs(budget2);
+
+        // Assert
+        assertFalse(result);
+    }
+}
