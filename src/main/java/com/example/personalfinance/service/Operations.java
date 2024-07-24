f5fb149d485a4e282c6cadba07e74ce29f577949
Pedro Silva <147632665+pedroswitch@users.noreply.github.com>
2024-07-24 11:46:53 +0100

    Converted from List to Iterable (findAll) and from String to long (delete).

--- src/main/java/com/example/personalfinance/service/Operations.java
+++ src/main/java/com/example/personalfinance/service/Operations.java
@@ -1,11 +1,7 @@
 package com.example.personalfinance.service;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface Operations<T>
 {
-    Optional<T> add(T item);
-    boolean delete(String id);
-    List<T> findAll();
+    boolean delete(long id);
+    Iterable<T> findAll();
 }
