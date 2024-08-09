e9002d6de350e5bc8053f94db70d693bd288f43f
Pedro Silva <147632665+pedroswitch@users.noreply.github.com>
2024-08-09 11:34:36 +0100

    Add methods to IncomeMapper.

--- src/main/java/com/example/personalfinance/mapper/IncomeMappper.java
+++ src/main/java/com/example/personalfinance/mapper/IncomeMappper.java
@@ -2,6 +2,8 @@ package com.example.personalfinance.mapper;
 
 import com.example.personalfinance.domain.income.Income;
 import com.example.personalfinance.domain.income.IncomeFactory;
+import com.example.personalfinance.domain.income.Salary;
+import com.example.personalfinance.domain.income.SideGig;
 import com.example.personalfinance.domain.valueobjects.*;
 import com.example.personalfinance.persistence.datamodel.IncomeDataModel;
 import com.example.personalfinance.persistence.datamodel.SalaryDataModel;
@@ -35,7 +37,7 @@ public class IncomeMappper
             return incomeFactory.createIncome(id, type, date, value, name);
         }
 
-        return incomeFactory.createIncome(id, type, date, value);
+        return null;
     }
 
     public static Iterable<Income> incomesDataModelToDomain(Iterable<IncomeDataModel> incomeDataModel)
@@ -45,5 +47,63 @@ public class IncomeMappper
                 dataModel -> incomes.add(incomeDataModelToDomain(dataModel))
         );
         return incomes;
+    }
+
+    public static IncomeDto salaryToIncomeDto(Income income)
+    {
+        Salary salary = (Salary) income;
+        return new IncomeDto(salary.getId().getId(), salary.getType().getType(), salary.getDate().getDate(), salary.getName().getName(), salary.getValue().getValue());
+    }
+
+    public static IncomeDto sideGigToIncomeDto(Income income)
+    {
+        SideGig sideGig = (SideGig) income;
+        return new IncomeDto(sideGig.getId().getId(), sideGig.getType().getType(), sideGig.getDate().getDate(), sideGig.getValue().getValue(), sideGig.getName().getName());
+    }
+
+    public static Iterable<IncomeDto> salaryListToDto(Iterable<Income> salaries)
+    {
+        List<IncomeDto> incomeListDto = new ArrayList<>();
+
+        salaries.forEach(salary -> {
+            IncomeDto incomeDto = salaryToIncomeDto(salary);
+            incomeListDto.add(incomeDto);
+        });
+
+        return incomeListDto;
+    }
+
+    public static Iterable<IncomeDto> sideGigListToDto(Iterable<Income> sideGigs)
+    {
+        List<IncomeDto> incomeListDto = new ArrayList<>();
+
+        sideGigs.forEach(sideGig -> {
+            IncomeDto incomeDto = sideGigToIncomeDto(sideGig);
+            incomeListDto.add(incomeDto);
+        });
+
+        return incomeListDto;
+    }
+
+    public static Income dtoToSalary(IncomeDto dto)
+    {
+        IncomeFactory incomeFactory = new IncomeFactory();
+        IncomeId id = new IncomeId(dto.id);
+        IncomeType type = new IncomeType(dto.type);
+        Date date = new Date(dto.date);
+        Values value = new Values(dto.value);
+        EmployerName name = new EmployerName(dto.employerName);
+        return incomeFactory.createIncome(id, type, date, value, name);
+    }
+
+    public static Income dtoToSideGig(IncomeDto dto)
+    {
+        IncomeFactory incomeFactory = new IncomeFactory();
+        IncomeId id = new IncomeId(dto.id);
+        IncomeType type = new IncomeType(dto.type);
+        Date date = new Date(dto.date);
+        Values value = new Values(dto.value);
+        SideGigName name = new SideGigName(dto.sideGigName);
+        return incomeFactory.createIncome(id, type, date, value, name);
     }
 }
