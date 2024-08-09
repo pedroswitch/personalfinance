155c620e6902d35c21c2c272dd7f40900286b2a7
Pedro Silva <147632665+pedroswitch@users.noreply.github.com>
2024-08-09 11:33:08 +0100

    Add @Service annotation to SalaryService.

--- src/main/java/com/example/personalfinance/service/SalaryService.java
+++ src/main/java/com/example/personalfinance/service/SalaryService.java
@@ -5,10 +5,12 @@ import com.example.personalfinance.domain.income.IncomeFactory;
 import com.example.personalfinance.domain.repository.IncomeRepo;
 import com.example.personalfinance.domain.valueobjects.*;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Optional;
 
+@Service
 public class SalaryService implements IncomeOperations<Income>
 {
     private final IncomeRepo incomeRepo;
